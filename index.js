03e76ac07baee0341e325912b7784c26980c5607
Emre <pi.emree@gmail.com>
2022-08-11 23:08:57 +0300

    update app

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const PORT = process.env.PORT || 5000;
 app.get("/api", (req, res) => {
   res.send("Hello World");
 });
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
