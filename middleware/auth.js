7f739f9d774d2a50a273db996c5b662e4ab1f683
Emre <pi.emree@gmail.com>
2022-08-12 00:54:03 +0300

    update api

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
-//   req.body?.apiPass === process.env.API_PASS
-//     ? next()
-//     : res.status(401).json({ error: "Unauthorized" });
-    next();
+  req.body?.apiPass === process.env.API_PASS
+    ? next()
+    : res.status(401).json({ error: "Unauthorized" });
 };
--- routes.js
+++ routes.js
@@ -4,11 +4,5 @@ const router = require("express").Router();
 
 router.post("/account", account);
 router.post("/balances", balances);
-router.get("/", (req, res) => {
-  res.send("Hello World2");
-});
-router.post("/", (req, res) => {
-  res.send(req.body);
-});
 
 module.exports = router;
