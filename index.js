f4ac0863269a8c5fc7166549a111ba0ede3532b1
Emre <pi.emree@gmail.com>
2022-08-11 23:11:57 +0300

    update app

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const routes = require("./routes");
 app.use(helmet());
 app.use(express.json());
 app.use("/api", routes);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
