78a16e3755a3051174ac7124620ddda26c314535
Emre <pi.emree@gmail.com>
2022-08-11 23:10:09 +0300

    update app

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-// const express = require("express");
-// const app = express();
-// const helmet = require("helmet");
-// const { port } = require("./config");
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const { port } = require("./config");
 
-// const routes = require("./routes");
+const routes = require("./routes");
 
-// app.use(helmet());
-// app.use(express.json());
-// app.use("/api", routes);
-
-// module.export = app;
-
-const app = require("express")();
-
-const PORT = process.env.PORT || 5000;
-
-app.get("/api", (req, res) => {
-  res.send("Hello World");
-});
-app.get("/", (req, res) => {
-    res.send("Hello World");
-  });
+app.use(helmet());
+app.use(express.json());
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
-})
+});
