bec929a5f566bc989256881f1cea19b8cdc309c8
Emre <pi.emree@gmail.com>
2022-08-11 23:04:04 +0300

    update app

--- index.js
+++ index.js
@@ -9,22 +9,12 @@
 // app.use(express.json());
 // app.use("/api", routes);
 
-
 // module.export = app;
 
-const app = require('express')();
-const { v4 } = require('uuid');
+const app = require("express")();
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.get("/api", (req, res) => {
+  res.send("Hello World");
 });
 
 module.exports = app;
