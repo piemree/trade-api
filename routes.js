31874fd87d4822b3269a7d85e26e3f47c35e19c0
Emre <pi.emree@gmail.com>
2022-08-12 00:48:35 +0300

    update api

--- routes.js
+++ routes.js
@@ -7,5 +7,8 @@ router.post("/balances", balances);
 router.get("/", (req, res) => {
   res.send("Hello World2");
 });
+router.post("/", (req, res) => {
+  res.send(req.body);
+});
 
 module.exports = router;
